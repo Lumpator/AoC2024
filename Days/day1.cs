8ab059cff15aec34ecffc1a915651f871ccc3e3c
Lumpator <vecisp@gmail.com>
2024-12-01 07:55:45 +0100

    little refactoring - changing variable names to be consistent

--- Days/day1.cs
+++ Days/day1.cs
@@ -4,45 +4,42 @@ namespace AoC2024.Days;
 
 public class Day1 : IDay
 {
-    private readonly string[] lines;
-    private List<int> leftValues;
-    private List<int> rightValues;
+    private readonly string[] _lines;
+    private List<int> _leftValues;
+    private List<int> _rightValues;
 
     public Day1()
     {
-        lines = Utils.Utilities.ReadLinesFromInputFIle("Inputs/inputDay1.txt");
-        leftValues = new List<int>();
-        rightValues = new List<int>();
+        _lines = Utils.Utilities.ReadLinesFromInputFIle("Inputs/inputDay1.txt");
+        _leftValues = new List<int>();
+        _rightValues = new List<int>();
         ParseInput();
     }
 
 
     public void SolvePart1()
     {
-        int Part1Result = 0;
+        int part1Result = 0;
 
-        leftValues.Sort();
-        rightValues.Sort();
+        _leftValues.Sort();
+        _rightValues.Sort();
 
-        for (int i = 0; i < leftValues.Count; i++)
+        for (int i = 0; i < _leftValues.Count; i++)
         {
-            int left = leftValues[i];
-            int right = rightValues[i];
-
-            Part1Result += Math.Abs(left - right);
+            part1Result += Math.Abs(_leftValues[i] - _rightValues[i]);
         }
 
-        Console.WriteLine($"Part 1 solution is: {Part1Result}");
+        Console.WriteLine($"Part 1 solution is: {part1Result}");
     }
 
     public void SolvePart2()
     {
-        long Part2Result = 0;
+        long part2Result = 0;
 
-        var rightValuesGrouped = rightValues.GroupBy(num => num).ToDictionary(num => num.Key, num => num.Count());
+        var rightValuesGrouped = _rightValues.GroupBy(num => num).ToDictionary(num => num.Key, num => num.Count());
         var leftValuesOccurences =
 
-        leftValues.Select(num => new
+        _leftValues.Select(num => new
         {
             Value = num,
             Count = rightValuesGrouped.ContainsKey(num) ? rightValuesGrouped[num] : 0
@@ -50,21 +47,21 @@ public class Day1 : IDay
 
         foreach (var leftValueOccurence in leftValuesOccurences)
         {
-            Part2Result += leftValueOccurence.Value * leftValueOccurence.Count;
+            part2Result += leftValueOccurence.Value * leftValueOccurence.Count;
         }
 
-        Console.WriteLine($"Part 2 solution is: {Part2Result} ");
+        Console.WriteLine($"Part 2 solution is: {part2Result} ");
     }
 
     private void ParseInput()
     {
-        foreach (string line in lines)
+        foreach (string line in _lines)
         {
             string[] parts = line.Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
             if (parts.Length == 2)
             {
-                leftValues.Add(int.Parse(parts[0]));
-                rightValues.Add(int.Parse(parts[1]));
+                _leftValues.Add(int.Parse(parts[0]));
+                _rightValues.Add(int.Parse(parts[1]));
             }
             else
             {
