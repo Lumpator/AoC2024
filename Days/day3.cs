4dcb0ec8bd1f5fae361fb3f59be39a466932fd08
Lumpator <vecisp@gmail.com>
2024-12-02 19:25:03 +0100

    prepared template for day3

---
+++ Days/day3.cs
@@ -0,0 +1,25 @@
+using AoC2024.Interfaces;
+
+namespace AoC2024.Days;
+
+public class Day3 : IDay
+{
+    private readonly string[] _lines;
+    public Day3()
+    {
+        _lines = File.ReadAllLines("Inputs/inputDay3.txt");
+    }
+
+    public void SolvePart1()
+    {
+        int part1Result = 0;
+        Console.WriteLine($"Part 1 solution is: {part1Result}");
+    }
+
+    public void SolvePart2()
+    {
+        int part2Result = 0;
+        Console.WriteLine($"Part 2 solution is: {part2Result} ");
+    }
+
+}
