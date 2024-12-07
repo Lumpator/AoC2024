48f1474e3a5d29f9f060ac3d2f9b754d5eead0cb
petr-vecera <petr.vecera@thermofisher.com>
2024-12-07 22:44:10 +0100

    refactoring and code cleaing for day6

--- Days/day6.cs
+++ Days/day6.cs
@@ -5,10 +5,8 @@ namespace AoC2024.Days;
 public class Day6 : IDay
 {
     private readonly string[] _lines;
-    private string _currentDirection;
 
-    private (int, int) _currentLocation;
-    private (int, int) _nextLocation;
+    private (string, (int, int)) _startingInfo;
 
     private List<((int, int), string)> _alreadyVisitedLocations = new List<((int, int), string)>();
 
@@ -22,50 +20,36 @@ public class Day6 : IDay
             _grid[i] = _lines[i].ToCharArray();
         }
 
-        var startingInfo = FindStartingDirectionAndLocation(_grid);
-        _currentDirection = startingInfo.Item1;
-        _currentLocation = startingInfo.Item2;
+        _startingInfo = FindStartingDirectionAndLocation(_grid);
+
     }
 
     public void SolvePart1()
     {
         int part1Result = 0;
+        var currentDirection = _startingInfo.Item1;
+        var currentLocation = _startingInfo.Item2;
+        (int, int) nextLocation = (0, 0);
 
         int MoveUntilLeavingGrid()
         {
             int steps = 1;
 
-            while (_currentLocation.Item1 >= 0 && _currentLocation.Item1 < _grid.Length && _currentLocation.Item2 >= 0 && _currentLocation.Item2 < _grid[0].Length)
+            while (CheckIfNextLocationIsInGridBoundary(currentLocation))
             {
-                _grid[_currentLocation.Item1][_currentLocation.Item2] = 'X';
-                switch (_currentDirection)
-                {
-                    case "up":
-                        _nextLocation = (_currentLocation.Item1 - 1, _currentLocation.Item2);
-                        break;
-                    case "down":
-                        _nextLocation = (_currentLocation.Item1 + 1, _currentLocation.Item2);
-                        break;
-                    case "left":
-                        _nextLocation = (_currentLocation.Item1, _currentLocation.Item2 - 1);
-                        break;
-                    case "right":
-                        _nextLocation = (_currentLocation.Item1, _currentLocation.Item2 + 1);
-                        break;
-                }
+                _grid[currentLocation.Item1][currentLocation.Item2] = 'X';
+                nextLocation = FindNextLocation(currentLocation, currentDirection, nextLocation);
 
-                if (!CheckIfNextLocationIsInGridBoundary(_nextLocation))
+                if (!CheckIfNextLocationIsInGridBoundary(nextLocation))
                 {
                     return steps;
                 }
-
                 // Go straight if possible or change your direction right
-                var moveresult = Move(_currentDirection, _currentLocation, _nextLocation);
-                _currentDirection = moveresult.Item1;
-                _currentLocation = moveresult.Item2;
+                var moveresult = Move(currentDirection, currentLocation, nextLocation);
+                currentDirection = moveresult.Item1;
+                currentLocation = moveresult.Item2;
 
-
-                if (_grid[_currentLocation.Item1][_currentLocation.Item2] != 'X')
+                if (_grid[currentLocation.Item1][currentLocation.Item2] != 'X')
                 {
                     steps++;
                 }
@@ -74,49 +58,33 @@ public class Day6 : IDay
             return steps;
         }
 
-        //part1Result += MoveUntilLeavingGrid();
+        part1Result += MoveUntilLeavingGrid();
         Console.WriteLine($"Part 1 solution is: {part1Result}");
     }
 
     public void SolvePart2()
     {
         int part2Result = 0;
-        HashSet<(int, int)> blockadeLocations = new HashSet<(int, int)>();
-
+        var currentDirection = _startingInfo.Item1;
+        var currentLocation = _startingInfo.Item2;
 
         int MoveUntilLeavingGrid((int, int) currentLocation, string currentDirection)
         {
             int paradoxCount = 0;
             var nextLocation = (0, 0);
 
-            while (currentLocation.Item1 >= 0 && currentLocation.Item1 < _grid.Length && currentLocation.Item2 >= 0 && currentLocation.Item2 < _grid[0].Length)
+            while (CheckIfNextLocationIsInGridBoundary(currentLocation))
             {
                 _alreadyVisitedLocations.Add((currentLocation, currentDirection));
 
-                switch (currentDirection)
-                {
-                    case "up":
-                        nextLocation = (currentLocation.Item1 - 1, currentLocation.Item2);
-                        break;
-                    case "down":
-                        nextLocation = (currentLocation.Item1 + 1, currentLocation.Item2);
-                        break;
-                    case "left":
-                        nextLocation = (currentLocation.Item1, currentLocation.Item2 - 1);
-                        break;
-                    case "right":
-                        nextLocation = (currentLocation.Item1, currentLocation.Item2 + 1);
-                        break;
-                }
+                nextLocation = FindNextLocation(currentLocation, currentDirection, nextLocation);
 
                 if (!CheckIfNextLocationIsInGridBoundary(nextLocation))
                 {
                     return paradoxCount;
                 }
 
-                // Check if adding '#' to the next location creates a cycle
-
-
+                // Check if adding '#' to the next location creates a paradox
                 if (_grid[nextLocation.Item1][nextLocation.Item2] != '#')
                 {
                     var gridCopy = _grid[nextLocation.Item1][nextLocation.Item2];
@@ -128,7 +96,6 @@ public class Day6 : IDay
                         if (!exists)
                         {
                             paradoxCount++;
-                            blockadeLocations.Add(nextLocation);
                         }
 
                     }
@@ -143,46 +110,21 @@ public class Day6 : IDay
             return paradoxCount;
         }
 
-        part2Result += MoveUntilLeavingGrid(_currentLocation, _currentDirection);
-        Console.WriteLine($"Part 2 solution is: {part2Result} ");
-        Console.WriteLine($"Blockade locations: {blockadeLocations.Count}");
+        part2Result += MoveUntilLeavingGrid(currentLocation, currentDirection);
+        Console.WriteLine($"Part 2 solution is: {part2Result}");
 
     }
 
     bool MoveUntilLeavingGridOrCreatingParadox((int, int) currentLocation, string currentDirection)
+    // retunrs true if finds paradox or false, if leaves the grid = no paradox
     {
 
         var nextLocation = (0, 0);
-        // make copy of _grid[][]
-        char[][] newGrid = new char[_grid.Length][];
-        for (int i = 0; i < _grid.Length; i++)
-        {
-            newGrid[i] = new char[_grid[i].Length];
-            Array.Copy(_grid[i], newGrid[i], _grid[i].Length);
-        }
-
-
         HashSet<((int, int), string)> visitedLocatinosWhenTryingToMakeParadox = new();
 
-        while (currentLocation.Item1 >= 0 && currentLocation.Item1 < _grid.Length && currentLocation.Item2 >= 0 && currentLocation.Item2 < _grid[0].Length)
+        while (CheckIfNextLocationIsInGridBoundary(currentLocation))
         {
-
-            switch (currentDirection)
-            {
-                case "up":
-                    nextLocation = (currentLocation.Item1 - 1, currentLocation.Item2);
-                    break;
-                case "down":
-                    nextLocation = (currentLocation.Item1 + 1, currentLocation.Item2);
-                    break;
-                case "left":
-                    nextLocation = (currentLocation.Item1, currentLocation.Item2 - 1);
-                    break;
-                case "right":
-                    nextLocation = (currentLocation.Item1, currentLocation.Item2 + 1);
-                    break;
-            }
-
+            nextLocation = FindNextLocation(currentLocation, currentDirection, nextLocation);
 
             if (!CheckIfNextLocationIsInGridBoundary(nextLocation))
             {
@@ -196,39 +138,38 @@ public class Day6 : IDay
 
             visitedLocatinosWhenTryingToMakeParadox.Add((currentLocation, currentDirection));
 
-
             // Go straight if possible or change your direction right
             var moveResult = Move(currentDirection, currentLocation, nextLocation);
             currentDirection = moveResult.Item1;
             currentLocation = moveResult.Item2;
-
-
-            switch (currentDirection)
-            {
-                case "up":
-                    newGrid[currentLocation.Item1][currentLocation.Item2] = '^';
-                    break;
-                case "down":
-                    newGrid[currentLocation.Item1][currentLocation.Item2] = 'v';
-                    break;
-                case "left":
-                    newGrid[currentLocation.Item1][currentLocation.Item2] = '<';
-                    break;
-                case "right":
-                    newGrid[currentLocation.Item1][currentLocation.Item2] = '>';
-                    break;
-            }
-
         }
         return false;
-
     }
 
+    private static (int, int) FindNextLocation((int, int) currentLocation, string currentDirection, (int, int) nextLocation)
+    {
+        switch (currentDirection)
+        {
+            case "up":
+                nextLocation = (currentLocation.Item1 - 1, currentLocation.Item2);
+                break;
+            case "down":
+                nextLocation = (currentLocation.Item1 + 1, currentLocation.Item2);
+                break;
+            case "left":
+                nextLocation = (currentLocation.Item1, currentLocation.Item2 - 1);
+                break;
+            case "right":
+                nextLocation = (currentLocation.Item1, currentLocation.Item2 + 1);
+                break;
+        }
+
+        return nextLocation;
+    }
 
     private (string, (int, int)) FindStartingDirectionAndLocation(char[][] grid)
     {
         (int, int) currentLocation = (-1, -1);
-        // Find the starting direction
         for (int i = 0; i < grid.Length; i++)
         {
             for (int j = 0; j < grid[i].Length; j++)
